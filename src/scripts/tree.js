38359f8f7cc6c9b8c586393d834db875c155015a
WujecDamian <wujecdamian12@gmail.com>
2026-03-23 13:23:13 +0100

    feat: write implementation of includes, insert & deleteItem functions

--- src/scripts/tree.js
+++ src/scripts/tree.js
@@ -1,12 +1,47 @@
 import { buildTree } from "./buildTree.js"
+import { node } from "./node.js";
 
 export function tree(arr){
     const uniqueSortedArray=[...new Set(arr.sort((a,b)=>a-b))]
     return{
         root:buildTree(uniqueSortedArray,0,uniqueSortedArray.length-1),
-        includes(value){},
-        insert(value){},
-        deleteItem(value){},
-        
+        includes(value){
+            let wasInside=false
+            uniqueSortedArray.forEach(element => {
+                if(element===value){
+                    wasInside = true
+                }
+            });
+            return wasInside
+        },
+        insert(root,value){
+
+            if(root===null){
+                return node(value)
+            }
+
+            if(value<root.root){
+                root.left=this.insert(root.left,value)
+            }
+            else{
+                root.right=this.insert(root.right,value)
+
+            }
+            return root
+        },
+        deleteItem(value){
+            if(!this.includes(value)) return
+            function findRoot(root,value){
+                if(root.left!==null){
+                    if(root.left===value){
+                        console.log('found it!')
+                    }
+                    else{
+                        
+                    }
+                }
+            }
+        },
+
     }
 }
