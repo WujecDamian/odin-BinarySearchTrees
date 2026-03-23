12fcbcc26602b1bdb73a421e9e8ccacfc943de43
WujecDamian <wujecdamian12@gmail.com>
2026-03-23 11:46:50 +0100

    refactor: change array to longer and with duplicates inside index tree1 object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './styles.css'
 import { tree } from './scripts/tree.js'
 import { prettyPrint } from './scripts/prettyPrint.js'
 
-let tree1=tree([1,9,3,5,7])
+let tree1=tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9,9,9,9,9, 67, 6345, 324])
 console.log(tree1.root)
 prettyPrint(tree1.root)
