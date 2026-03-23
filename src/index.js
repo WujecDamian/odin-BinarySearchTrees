c5b4cceadd6da227e0776ebc94967adcb7f280da
WujecDamian <wujecdamian12@gmail.com>
2026-03-23 13:23:52 +0100

    test: add initial tree insertions and pretty print demo to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './styles.css'
 import { tree } from './scripts/tree.js'
 import { prettyPrint } from './scripts/prettyPrint.js'
 
-let tree1=tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9,9,9,9,9, 67, 6345, 324])
-console.log(tree1.root)
+let tree1=tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 9, 9, 9, 9, 67, 6345, 324])
+tree1.insert(tree1.root,'11')
+tree1.insert(tree1.root,'2')
+tree1.insert(tree1.root,'86')
+tree1.insert(tree1.root,'432')
+
 prettyPrint(tree1.root)
