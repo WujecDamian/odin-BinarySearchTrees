4b6953785b8bb000a75521569d4a2db480eb2281
WujecDamian <wujecdamian12@gmail.com>
2026-03-23 11:10:28 +0100

    feat: add parameters to buildTree call inside tree factory

--- src/scripts/tree.js
+++ src/scripts/tree.js
@@ -1,6 +1,7 @@
+import { buildTree } from "./buildTree.js"
 
 export function tree(arr){
     return{
-        root:buildTree()
+        root:buildTree(arr.sort((a,b)=>a-b),0,arr.length-1)
     }
 }
