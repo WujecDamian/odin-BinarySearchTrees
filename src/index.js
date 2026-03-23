84164748747c9c5e1b4d631c335ede1e4b447c74
WujecDamian <wujecdamian12@gmail.com>
2026-03-23 11:11:00 +0100

    feat: add imports and call for debugging

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 //put inside src/index.js
 import './styles.css'
+import { tree } from './scripts/tree.js'
+import { prettyPrint } from './scripts/prettyPrint.js'
+
+let tree1=tree([1,9,3,5,7])
+console.log(tree1.root)
+prettyPrint(tree1.root)
