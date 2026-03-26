3e42ceb3afcae798b0891c2063ee2151231b2483
WujecDamian <wujecdamian12@gmail.com>
2026-03-26 21:48:22 +0100

    test: test rebalancing with inserted values [X] TEST NOT PASSED

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ import { prettyPrint } from "./scripts/prettyPrint.js";
 
 let tree1 = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 9, 9, 9, 9, 67, 6345, 324]);
 /* tree1.insert(tree1.root,'11')
-tree1.insert(tree1.root,'2')
-tree1.insert(tree1.root,'86')
-tree1.insert(tree1.root,'432')
+
 tree1.deleteItem(tree1.root, 4); 
 tree1.levelOrderForEach(tree1.callback);
 tree1.preOrderForEach(tree1.callback);
 tree1.postOrderForEach(tree1.callback);
 console.log(`${tree1.depth(tree1.root, 9)}`);
 console.log(`${tree1.height(tree1.root, 9)}`);*/
-console.log(`${tree1.isBalanced()}`);
+tree1.insert(tree1.root, 2);
+tree1.insert(tree1.root, 86);
+tree1.insert(tree1.root, 432);
+tree1.insert(tree1.root, 2);
+tree1.insert(tree1.root, 86);
+tree1.insert(tree1.root, 433);
+
+tree1.insert(tree1.root, 434);
 
 tree1.rebalance();
 prettyPrint(tree1.root);
