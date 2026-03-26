afd88bd7ce6f26c87cc258efc0931beab40c58e7
WujecDamian <wujecdamian12@gmail.com>
2026-03-26 12:45:53 +0100

    feat: add inOrderForEach function.

--- src/scripts/tree.js
+++ src/scripts/tree.js
@@ -191,11 +191,39 @@ export function tree(arr) {
 
         recursiveTraverse(queue[0], callback);
       }
-    },
+    }, //!IN ORDER---------------------------------------------------------------
     inOrderForEach(callback) {
       //check if left is not empty
       //recurse on left
-    },
+      if (typeof callback !== "function") {
+        throw new Error(
+          "Provided parameter is not a function! (Expected: callback function)",
+        );
+      }
+
+      recursiveTraverse(this.root, callback);
+      function recursiveTraverse(node, callback) {
+        if (
+          node != undefined &&
+          node != null &&
+          node.left !== null &&
+          node.left !== undefined
+        ) {
+          recursiveTraverse(node.left, callback);
+        }
+        if (node !== undefined && node !== null) {
+          callback(node.root);
+        }
+        if (
+          node != undefined &&
+          node != null &&
+          node.right !== null &&
+          node.right !== undefined
+        ) {
+          recursiveTraverse(node.right, callback);
+        }
+      }
+    }, //!PRE ORDER---------------------------------------------------------------
     preOrderForEach(callback) {
       if (typeof callback !== "function") {
         throw new Error(
@@ -224,7 +252,7 @@ export function tree(arr) {
           recursiveTraverse(node.right, callback);
         }
       }
-    },
+    }, //!POST ORDER---------------------------------------------------------------
     postOrderForEach(callback) {},
   };
 }
