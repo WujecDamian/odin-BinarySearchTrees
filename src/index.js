0dd08d71c7e9d0cd90097f4278ca503ed94ac1a9
WujecDamian <wujecdamian12@gmail.com>
2026-03-26 09:31:03 +0100

    test: test levelOrderForEach function.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ let tree1 = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 9, 9, 9, 9, 67, 6345, 324]);
 /* tree1.insert(tree1.root,'11')
 tree1.insert(tree1.root,'2')
 tree1.insert(tree1.root,'86')
-tree1.insert(tree1.root,'432') */
-
-tree1.deleteItem(tree1.root, 4);
-
+tree1.insert(tree1.root,'432')
+tree1.deleteItem(tree1.root, 4); */
+tree1.levelOrderForEach(tree1.callback);
 prettyPrint(tree1.root);
